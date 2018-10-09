b33d2dcadd2845fbeddc7808175bc8c43012dc84
felipe fanucchi <felipefanucchi2010@hotmail.com>
2018-10-08 22:40:10 -0300

    Rota aceitando valores dinamicos.

--- app/src/app/photos/photo-list/photo-list.component.ts
+++ app/src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Photo } from '../photo/photo.interface';
 import { PhotoService } from '../photo/photo.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photos-list',
@@ -11,10 +12,13 @@ export class PhotoListComponent implements OnInit {
   
   photos: Photo[];
   
-  constructor(private photosService: PhotoService) {}
+  constructor(
+    private photosService: PhotoService,
+    private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.photosService.listFromUser()
+    const user = this.activatedRoute.snapshot.params.userName;
+    this.photosService.listFromUser(user)
       .subscribe(photos =>  {
         this.photos = photos;
     });
